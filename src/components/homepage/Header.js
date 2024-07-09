c64e235c3b4e82010dce2fbd58f7f61cd7869d5b
Yehdar <p.radhey06@gmail.com>
2024-07-09 16:24:39 -0400

    homepage: add annoucement and unique header components

--- src/components/homepage/Header.js
+++ src/components/homepage/Header.js
@@ -8,12 +8,12 @@ const Header = () => {
     <header className="bg-lassonde-grey text-slate-100 p-4 border-t-2 border-neutral-700">
       <div className="container mx-auto px-5">
         <h1 className="text-4xl font-bold mb-2">Lassonde Student Congress</h1>
-        {/* <p className="mb-2">
+        <p className="mb-2">
           The Lassonde Student Congress had two Chambers and a Secretariat that
           work on behalf of the Lassonde Community to provide services,
           challenge decisions of the Faculty Council, make and shape
           policies/laws, and advocate on key issues of the day.
-        </p> */}
+        </p>
         {/* <nav className="text-green-600">
           <a href="#" className="hover:underline">
             1
