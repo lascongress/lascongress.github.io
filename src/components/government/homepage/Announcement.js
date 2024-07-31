91da55b1a1ab76d4bf1640a3b8c12b1432ceb913
Yehdar <p.radhey06@gmail.com>
2024-07-31 17:38:44 -0400

    government-homepage-feat: introduce annoucements

--- src/components/government/homepage/Announcement.js
+++ src/components/government/homepage/Announcement.js
@@ -8,10 +8,10 @@ const Announcement = () => {
     <section className="bg-yellow-100 p-4 mb-6 rounded">
       <h2 className="font-bold text-lg">General election 2024</h2>
       <p>
-        The LSC general election takes place July 2024. The new Parliament has
-        been called to meet on Tuesday 9 July 2024 when the business will be the
-        election of the Speaker. The State Opening of Parliament and The s
-        Speech will follow on Wednesday 17 July 2024.
+        The LSC general election takes place August 2024. The new Parliament has
+        been called to meet on Monday 29 July 2024 when the business will be the
+        second house meeting. The State Opening of Parliament and The State
+        Speech will follow on Friday 2 August 2024.
       </p>
     </section>
   );
--- src/components/government/homepage/MainContent.js
+++ src/components/government/homepage/MainContent.js
@@ -2,14 +2,14 @@
 import React from "react";
 // import News from "./news/News";
 import Links from "./links/Links";
-// import Announcement from "./Announcement";
+import Announcement from "./Announcement";
 
 const MainContent = () => {
   return (
     <div className="container mx-auto p-4">
-      {/* <Announcement /> */}
+      <Announcement />
       {/* <News /> */}
-      <Links />
+      {/* <Links /> */}
     </div>
   );
 };
