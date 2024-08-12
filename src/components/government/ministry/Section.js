203cee47903cd6da6dab99c8b1ab4111fd4e30eb
Yehdar <p.radhey06@gmail.com>
2024-08-11 20:03:02 -0400

    government: add ministry

---
+++ src/components/government/ministry/Section.js
@@ -0,0 +1,156 @@
+// src/components/government/ministry/Section.js
+"use client";
+
+import React from "react";
+import Card from "./Card";
+
+const Section = () => {
+  const section = [];
+
+  return (
+    <section className="bg-white p-4 rounded shadow">
+      <div className="container mx-auto p-4 mb-4 border-b-2 border-neutral-200">
+        <h2 className="text-3xl font-bold mb-2 text-gray-800">Ministry</h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">
+          Class A Committee
+        </h2>
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Executive Committee (EC)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Planning, Academic Resources & Research Committee (PARR)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Learning, Curriculum & Students Committee (LCS)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <br />
+
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">
+          Class B Committee
+        </h2>
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Committee On Evaluations & Academic Standards (CEAS-M)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Committee On Evaluations & Academic Standards - Academic Honesty Panel
+          (CEAS-AHP)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Committee On Evaluations & Academic Standards - Academic Petitions
+          Subcomm (CEAS-APS)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <br />
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">
+          Class C Committee
+        </h2>
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Tenure & Promotions Committee (TPC)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Awards Committee (AC)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <br />
+
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">
+          Class O Committee
+        </h2>
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Faculty Council At-Large
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {section.map((link, index) => (
+          <Card
+            key={index}
+            imageSrc={link.imageSrc}
+            title={link.title}
+            description={link.description}
+            link={link.link}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Section;
