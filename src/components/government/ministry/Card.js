88ffbdd3ceb6dca21a5fddadb0265636f4d88ff6
Yehdar <p.radhey06@gmail.com>
2024-09-03 19:40:40 -0400

    ministry-feat: improve user interface

--- src/components/government/ministry/Card.js
+++ src/components/government/ministry/Card.js
@@ -1,23 +1,41 @@
 // src/components/government/ministry/Card.js
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
-const Card = ({ imageSrc, title, description, link }) => {
+const Card = ({
+  iconSrc,
+  departmentName,
+  linkText,
+  linkHref,
+  dropdownItems,
+}) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <Link href={link}>
-      <div className="bg-white p-4 rounded shadow hover:bg-gray-100 transition duration-200 flex flex-col h-80">
-        {/* adjust the height in increments of +/- 8 */}
-        <img
-          src={imageSrc}
-          alt={title}
-          className="mb-4 w-full h-32 object-cover rounded"
-        />
-        <div className="flex justify-between items-center mb-2">
-          <h3 className="font-bold text-lg">{title}</h3>
+    <div className="flex flex-col items-start">
+      <img src={iconSrc} alt={departmentName} />
+      <h3 className="text-2xl font-bold mb-1">
+        <Link href={linkHref} className="hover:underline">
+          {departmentName}
+        </Link>
+      </h3>
+      Dropdown Link
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="text-blue-600 hover:underline flex items-center"
+        >
+          <span className="mr-2">{linkText}</span>
           <svg
-            className="h-5 w-5"
+            className={`h-4 w-4 text-blue-600 transform ${
+              isDropdownOpen ? "rotate-180" : "rotate-0"
+            }`}
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
@@ -27,13 +45,27 @@ const Card = ({ imageSrc, title, description, link }) => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M9 5l7 7-7 7"
+              d="M5 12h14m-7-7l7 7-7 7"
             />
           </svg>
-        </div>
-        <p className="flex-grow">{description}</p>
+        </button>
+
+        {/* Dropdown menu this aint working lmao im gonna cry*/}
+        {isDropdownOpen && (
+          <div className="absolute mt-2 bg-white border border-gray-300 rounded shadow-lg">
+            <ul>
+              {dropdownItems.map((item, index) => (
+                <li key={index} className="px-4 py-2 hover:bg-gray-100">
+                  <Link href={item.href}>
+                    <a className="text-blue-600 hover:underline">{item.text}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
-    </Link>
+    </div>
   );
 };
 
--- src/components/government/ministry/Section.js
+++ src/components/government/ministry/Section.js
@@ -2,85 +2,60 @@
 "use client";
 
 import React from "react";
-import Card from "./Card";
 
-const Section = () => {
-  const special = [
-    {
-      imageSrc: "/images/office-of-the-secretariat.png",
-      title: "Office Of The Secretariat",
-      description:
-        "Keep up to date with the business of the House by referring to our Agenda & Minutes, Votes and other communications.",
-      link: "/government/resources/student-run",
-    },
-  ];
-  const section = [
-    {
-      imageSrc: "/images/department-of-internal-affairs.png",
-      title: "Department of Internal Affairs",
-      description:
-        "Keep up to date with the business of the House by referring to our Agenda & Minutes, Votes and other communications.",
-      link: "/government/resources/student-run",
-    },
-    {
-      imageSrc: "/images/department-of-community-affairs.png",
-      title: "Department of Community Affairs",
-      description:
-        "The House of Commons is governed by a group of Delegates who make up the Steering Committee.",
-      link: "/government/resources/student-run",
-    },
-    {
-      imageSrc: "/images/department-of-finance-and-admin.png",
-      title: "Department of Finance And Admin",
-      description:
-        "A lot of the work and discussions of the House takes place in committees. The committee examines issues in detail, from proposed bills  to wider topics of community concern.",
-      link: "/government/resources/student-run",
-    },
-    {
-      imageSrc: "/images/office-of-the-accountability.png",
-      title: "Office of Accountability",
-      description:
-        "Watch all House meetings online and find archived coverage.",
-      link: "/government/resources/student-run",
-    },
-  ];
-
+const Section = ({ title, departments, count }) => {
   return (
-    <section className="bg-white p-4 rounded shadow">
-      <div className="container mx-auto p-4 mb-4 border-b-2 border-neutral-200">
-        <h2 className="text-3xl font-bold mb-2 text-gray-800">Services</h2>
-        <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
-        </p>
+    <div className="bg-white p-6 mb-6">
+      <div className="flex flex-col md:flex-row justify-between items-start min-h-[300px]">
+        <div className="flex-shrink-0 mb-4 md:mb-0 mr-4 min-w-[250px]">
+          <p className="text-xl font-bold">{title}</p>
+          <p className="text-5xl font-bold">{count}</p>
+        </div>
+        <div className="flex-grow md:pl-12 border-t-2 md:border-t-0 md:border-l-2 border-gray-200">
+          {departments.map((department, index) => (
+            <div
+              key={index}
+              className="mb-4 pb-4 border-b last:border-b-0 flex flex-col items-center md:items-start"
+            >
+              <div className="flex-shrink-0 mb-2 md:mb-0">
+                <img
+                  src={department.iconSrc}
+                  alt={department.name}
+                  className="h-64 w-64"
+                />
+              </div>
+              <div className="flex-grow text-center md:text-left">
+                <p className="text-lg font-bold">
+                  <a href={department.linkHref} className="hover:underline">
+                    {department.name}
+                  </a>
+                </p>
+                <a
+                  href={department.linkHref}
+                  className="text-blue-600 hover:underline flex items-center justify-center md:justify-start mt-1"
+                >
+                  <span className="mr-2">{department.linkText}</span>
+                  <svg
+                    className="h-4 w-4 text-blue-600"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 12h14m-7-7l7 7-7 7"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-1 gap-4 pb-4">
-        {special.map((link, index) => (
-          <Card
-            key={index}
-            imageSrc={link.imageSrc}
-            title={link.title}
-            description={link.description}
-            link={link.link}
-          />
-        ))}
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {section.map((link, index) => (
-          <Card
-            key={index}
-            imageSrc={link.imageSrc}
-            title={link.title}
-            description={link.description}
-            link={link.link}
-          />
-        ))}
-      </div>
-    </section>
+    </div>
   );
 };
 
