04fc8e833dbb3f9854f1f4cc4f1700400fb373b6
Yehdar <p.radhey06@gmail.com>
2024-10-06 19:52:02 -0400

    add new social media images

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="bg-lassonde-grey text-white p-8">
       <div className="container px-9 mx-auto grid lg:grid-cols-1 gap-8">
         <div>
-          <h3 className="font-bold mb-2">Follow Us</h3>
+          <h3 className="font-bold mb-4">Follow Us</h3>
           <div className="flex flex-wrap md:flex-row md:space-x-16 mb-4">
             <a
               href="https://www.instagram.com/lassondecongress/"
@@ -21,6 +21,34 @@ const Footer = () => {
                 style={{ filter: "invert(1) brightness(2)" }} // Apply filter to make it white
               />
               <span>Instagram</span>
+            </a>
+          </div>
+          <div className="flex flex-wrap md:flex-row md:space-x-16 mb-4">
+            <a
+              href="https://www.instagram.com/lassondecongress/"
+              className="flex items-center space-x-2 hover:underline mb-2 md:mb-0"
+            >
+              <img
+                src="/social/discord.png"
+                alt="Instagram"
+                className="h-4"
+                // style={{ filter: "invert(1) brightness(2)" }} // Apply filter to make it white
+              />
+              <span>Discord</span>
+            </a>
+          </div>
+          <div className="flex flex-wrap md:flex-row md:space-x-16 mb-4">
+            <a
+              href="https://www.instagram.com/lassondecongress/"
+              className="flex items-center space-x-2 hover:underline mb-2 md:mb-0"
+            >
+              <img
+                src="/social/reddit.png"
+                alt="Reddit"
+                className="h-6"
+                style={{ filter: "invert(1) brightness(2)" }} // Apply filter to make it white
+              />
+              <span>Reddit</span>
             </a>
           </div>
           {/* <h3 className="font-bold mb-2">Join Us By Reaching Out</h3>
