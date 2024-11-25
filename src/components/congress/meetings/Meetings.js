b0a892398e28af7cd84320a2fa0572082173896c
Yehdar <p.radhey06@gmail.com>
2024-11-25 18:42:38 -0500

    congress/meetings/Meetings: add more meeting dates and fix var names

--- src/components/congress/meetings/Meetings.js
+++ src/components/congress/meetings/Meetings.js
@@ -3,12 +3,12 @@
 
 import React from "react";
 
-const Card = ({ time, title, date, committees, location, link }) => {
+const Card = ({ date, title, time, committees, location, link }) => {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer" className="block">
       <div className="bg-white p-4 rounded shadow mb-4 hover:bg-gray-100 transition duration-200">
         <div className="flex justify-between items-center mb-2">
-          <div className="text-xl font-bold text-gray-800">{time}</div>
+          <div className="text-xl font-bold text-gray-800">{date}</div>
           <svg
             className="h-5 w-5 text-gray-800"
             fill="none"
@@ -25,7 +25,7 @@ const Card = ({ time, title, date, committees, location, link }) => {
           </svg>
         </div>
         <h3 className="text-lg font-bold mb-1">{title}</h3>
-        <p className="text-gray-600">{date}</p>
+        <p className="text-gray-600">{time}</p>
         <p className="text-gray-600">{committees}</p>
         <p className="text-gray-600">{location}</p>
       </div>
@@ -62,9 +62,9 @@ const Section = ({ title, description, buttonLabel, cards, events }) => {
           events.map((event, index) => (
             <Card
               key={index}
-              time={event.time}
-              title={event.title}
               date={event.date}
+              title={event.title}
+              time={event.time}
               committees={event.committees}
               location={event.location}
               link={event.link}
@@ -93,54 +93,53 @@ const MainContent = () => {
 
   const rightSideEvents = [
     {
-      time: "Time: 6:00pm",
-      title: "Bi-weekly: Cabinet Meeting",
       date: "Nov 26th 2024",
+      title: "Bi-weekly: Cabinet Meeting",
+      time: "6:00pm",
       committees: "Executive Cabinet",
       location: "Location: Discord",
       link: "https://discord.com/events/1205712080385351710/1301302258696458240",
     },
     {
-      time: "Time: 8:15pm",
-      title: "Bi-weekly: Senate Meetings",
       date: "Nov 27th 2024",
+      title: "Bi-weekly: Senate Meetings",
+      time: "8:15pm",
       committees: "Members: Advocacy Committee",
       location: "Location: Discord",
       link: "https://discord.com/events/1205712080385351710/1306388075634753536",
     },
-
-    // {
-    //   time: "Aug 13th 2024",
-    //   title: "Advocacy Commitee Meeting",
-    //   date: "Time: 5:30pm",
-    //   committees: "Members: Advocacy Committee",
-    //   location: "Location: Discord",
-    //   link: "https://discord.gg/ZrVqTKhDHr?event=1271654755000913970",
-    // },
-    // {
-    //   time: "Aug 16th 2024",
-    //   title: "LSC Government Working Session",
-    //   date: "Time: 2:00pm",
-    //   committees: "Members: Government",
-    //   location: "Location: Discord",
-    //   link: "https://discord.gg/ntN69q9HfK?event=1268024577745748159",
-    // },
-    // {
-    //   time: "Aug 18th 2024",
-    //   title: "Biweekly Senate Meetings",
-    //   date: "Time: 6:30pm",
-    //   committees: "Members: Senate",
-    //   location: "Location: Discord",
-    //   link: "https://discord.gg/ZrVqTKhDHr?event=1265801755250130944",
-    // },
-    // {
-    //   time: "Aug 30th 2024",
-    //   title: "House of Delegates Meeting #3",
-    //   date: "Time: 6:00pm",
-    //   committees: "Members: House",
-    //   location: "Location: Discord",
-    //   link: "https://discord.gg/ZrVqTKhDHr?event=1271329331066703934",
-    // },
+    {
+      date: "Dec 10th 2024",
+      title: "Bi-weekly: Cabinet Meeting",
+      time: "6:00pm",
+      committees: "Executive Cabinet",
+      location: "Location: Discord",
+      link: "https://discord.com/events/1205712080385351710/1301302258696458240",
+    },
+    {
+      date: "Dec 11th 2024",
+      title: "Bi-weekly: Senate Meetings",
+      time: "8:15pm",
+      committees: "Members: Advocacy Committee",
+      location: "Location: Discord",
+      link: "https://discord.com/events/1205712080385351710/1306388075634753536",
+    },
+    {
+      date: "Dec 24th 2024",
+      title: "Bi-weekly: Cabinet Meeting",
+      time: "6:00pm",
+      committees: "Executive Cabinet",
+      location: "Location: Discord",
+      link: "https://discord.com/events/1205712080385351710/1301302258696458240",
+    },
+    {
+      date: "Dec 25th 2024",
+      title: "Bi-weekly: Senate Meetings",
+      time: "8:15pm",
+      committees: "Members: Advocacy Committee",
+      location: "Location: Discord",
+      link: "https://discord.com/events/1205712080385351710/1306388075634753536",
+    },
   ];
 
   return (
@@ -149,11 +148,8 @@ const MainContent = () => {
         <div className="container mx-auto p-4 mb-4 border-b-2 border-neutral-200">
           <h2 className="text-3xl font-bold mb-2 text-gray-800">Meetings</h2>
           <p className="text-lg text-gray-700">
-            The UK public elects 650 Members of Parliament (MPs) to represent
-            their interests and concerns in the House of Commons. MPs consider
-            and propose new laws, and can scrutinise government policies by
-            asking ministers questions about current issues either in the
-            Commons Chamber or in Committees.
+            Find all our meetings here! If you want to learn more, join our
+            discord!
           </p>
         </div>
       </div>
