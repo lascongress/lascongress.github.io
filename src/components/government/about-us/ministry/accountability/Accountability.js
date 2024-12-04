193dad008c6ef42073061d2e2a3f77c6dc56592e
Yehdar <p.radhey06@gmail.com>
2024-12-04 18:27:35 -0500

    government/about-us/ministry/*: add ministerial departments

---
+++ src/components/government/about-us/ministry/accountability/Accountability.js
@@ -0,0 +1,197 @@
+// src/components/congress/committees/Committees.js
+import React from "react";
+import Link from "next/link";
+
+const Card = ({ imageSrc, title, description, link }) => {
+  return (
+    <Link href={link}>
+      <div className="bg-white p-4 rounded shadow hover:bg-gray-100 transition duration-200 flex flex-col h-80">
+        {/* adjust the height in increments of +/- 8 */}
+        <img
+          src={imageSrc}
+          alt={title}
+          className="mb-4 w-full h-32 object-cover rounded"
+        />
+        <div className="flex justify-between items-center mb-2">
+          <h3 className="font-bold text-lg">{title}</h3>
+          <svg
+            className="h-5 w-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </div>
+        <p className="flex-grow">{description}</p>
+      </div>
+    </Link>
+  );
+};
+
+const Section = () => {
+  const section = [];
+
+  return (
+    <section className="bg-white p-4 rounded shadow">
+      <div className="container mx-auto p-4 mb-4 border-b-2 border-neutral-200">
+        <h2 className="text-3xl font-bold mb-2 text-gray-800">
+          Accountability
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">
+          Class A Committee
+        </h2>
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Executive Committee (EC)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Planning, Academic Resources & Research Committee (PARR)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Learning, Curriculum & Students Committee (LCS)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <br />
+
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">
+          Class B Committee
+        </h2>
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Committee On Evaluations & Academic Standards (CEAS-M)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Committee On Evaluations & Academic Standards - Academic Honesty Panel
+          (CEAS-AHP)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Committee On Evaluations & Academic Standards - Academic Petitions
+          Subcomm (CEAS-APS)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <br />
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">
+          Class C Committee
+        </h2>
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Tenure & Promotions Committee (TPC)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Awards Committee (AC)
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+        <br />
+        <br />
+
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">
+          Class O Committee
+        </h2>
+        <h2 className="text-xl font-bold mb-2 text-gray-800">
+          Faculty Council At-Large
+        </h2>
+        <p className="text-lg text-gray-700">
+          The UK public elects 650 Members of Parliament (MPs) to represent
+          their interests and concerns in the House of Commons. MPs consider and
+          propose new laws, and can scrutinise government policies by asking
+          ministers questions about current issues either in the Commons Chamber
+          or in Committees.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {section.map((link, index) => (
+          <Card
+            key={index}
+            imageSrc={link.imageSrc}
+            title={link.title}
+            description={link.description}
+            link={link.link}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+const MainContent = () => {
+  return (
+    <main className="container mx-auto p-4">
+      <Section />
+    </main>
+  );
+};
+
+export default MainContent;
---
+++ src/components/government/about-us/ministry/student-success/Breadcrumb.js
@@ -0,0 +1,23 @@
+// src/components/government/ministry/Breadcrumb.js
+import BreadcrumbNavBar from "../../../../global/BreadcrumbNavBar";
+
+export default function ExamplePage() {
+  const breadcrumbs = [
+    { label: "Main", href: "/" },
+    {
+      label: "Ministry",
+      href: "/government/about-us/ministry",
+    },
+    {
+      label: "Accountability",
+      href: "/government/about-us/ministry/student-success",
+    },
+  ];
+
+  return (
+    <div>
+      <BreadcrumbNavBar breadcrumbs={breadcrumbs} />
+      {/* Other page content */}
+    </div>
+  );
+}
