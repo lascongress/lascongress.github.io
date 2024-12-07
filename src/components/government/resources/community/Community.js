98013507d38b3d62b1a763104468ba095c76275d
Yehdar <p.radhey06@gmail.com>
2024-12-07 17:31:06 -0500

    congress/government/resources/Community: add content

--- src/components/government/resources/community/Community.js
+++ src/components/government/resources/community/Community.js
@@ -43,130 +43,119 @@ const Section = () => {
       <div className="container mx-auto p-4 mb-4 border-b-2 border-neutral-200">
         <h2 className="text-3xl font-bold mb-2 text-gray-800">Community</h2>
         <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
+          York University students are very talented and have produced many
+          applications to improve the life of students. To commerate and
+          showcase their work, this webpage is a collection of resources made by
+          York University students for York University students. Please note
+          that the following applications are owned by their respective
+          creators, and NOT Lassonde Student Congress. Therefore, all credit
+          should be given rigthfully to the creators.
         </p>
         <br />
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          Class A Committee
-        </h2>
-        <h2 className="text-xl font-bold mb-2 text-gray-800">
-          Executive Committee (EC)
+        <h2 className="text-xl font-bold text-gray-800">
+          <Link
+            href="https://coursedelta.yorku.dev/"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CourseDelta
+          </Link>
         </h2>
         <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
+          Atlas of Course Skill Trees/Prerequisite Graphs
         </p>
+        <p className="text-lg text-gray-700">By Kevin Y. H. Hui</p>
         <br />
-        <h2 className="text-xl font-bold mb-2 text-gray-800">
-          Planning, Academic Resources & Research Committee (PARR)
+        <h2 className="text-xl font-bold text-gray-800">
+          <Link
+            href="https://study.yorku.dev/"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            YUStudyBuddies
+          </Link>
         </h2>
         <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
+          Find group chats. Connect with classmates. Ace your courses.
         </p>
+        <p className="text-lg text-gray-700">By Jerry Chen</p>
         <br />
-        <h2 className="text-xl font-bold mb-2 text-gray-800">
-          Learning, Curriculum & Students Committee (LCS)
+        <h2 className="text-xl font-bold text-gray-800">
+          <Link
+            href="https://rooms.yorku.dev/"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Empty Rooms at YorkU
+          </Link>
+        </h2>
+        <p className="text-lg text-gray-700">Room Usage Schedule Finder</p>
+        <p className="text-lg text-gray-700">By Kevin Y. H. Hui</p>
+        <br />
+        <h2 className="text-xl font-bold text-gray-800">
+          <Link
+            href="https://find.yorku.dev/"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Class Find Tool
+          </Link>
         </h2>
         <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
+          A site to help students find their way around York University{"'"}s
+          campus.
         </p>
+        <p className="text-lg text-gray-700">Issac Kogen</p>
         <br />
-        <br />
-
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          Class B Committee
-        </h2>
-        <h2 className="text-xl font-bold mb-2 text-gray-800">
-          Committee On Evaluations & Academic Standards (CEAS-M)
+        <h2 className="text-xl font-bold text-gray-800">
+          <Link
+            href="https://www.ratemycourses.io/"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rate My Courses
+          </Link>
         </h2>
         <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
+          See reviews, get advice and find resources for university courses
         </p>
+        <p className="text-lg text-gray-700">By Oopsidaisy (discord)</p>
         <br />
-        <h2 className="text-xl font-bold mb-2 text-gray-800">
-          Committee On Evaluations & Academic Standards - Academic Honesty Panel
-          (CEAS-AHP)
+        <h2 className="text-xl font-bold text-gray-800">
+          <Link
+            href="https://github.com/mahfoozm/YorkURMP/releases"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            RateMyProf Extension
+          </Link>
         </h2>
         <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
+          VSB Overlay Plug-in to provide Professor ratings on course entires
         </p>
+        <p className="text-lg text-gray-700">By Mohammed Mahfooz</p>
         <br />
-        <h2 className="text-xl font-bold mb-2 text-gray-800">
-          Committee On Evaluations & Academic Standards - Academic Petitions
-          Subcomm (CEAS-APS)
+        <h2 className="text-xl font-bold text-gray-800">
+          <Link
+            href="https://chrisnguyn.github.io/york-cs-primer/"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            York CS Primer
+          </Link>
         </h2>
         <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
+          Alumni Lassonde Computer Science (unofficial) guide
         </p>
+        <p className="text-lg text-gray-700">By Chris Nguyen</p>
         <br />
-        <br />
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          Class C Committee
-        </h2>
-        <h2 className="text-xl font-bold mb-2 text-gray-800">
-          Tenure & Promotions Committee (TPC)
-        </h2>
-        <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
-        </p>
-        <br />
-        <h2 className="text-xl font-bold mb-2 text-gray-800">
-          Awards Committee (AC)
-        </h2>
-        <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
-        </p>
-        <br />
-        <br />
-
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          Class O Committee
-        </h2>
-        <h2 className="text-xl font-bold mb-2 text-gray-800">
-          Faculty Council At-Large
-        </h2>
-        <p className="text-lg text-gray-700">
-          The UK public elects 650 Members of Parliament (MPs) to represent
-          their interests and concerns in the House of Commons. MPs consider and
-          propose new laws, and can scrutinise government policies by asking
-          ministers questions about current issues either in the Commons Chamber
-          or in Committees.
-        </p>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
